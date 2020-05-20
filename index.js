57eae09717708fd51c4d0c08e6c6fc6d4eff991f
wuweixing <1149308443@qq.com>
2020-05-20 18:04:49 +0800

    'init基础版本实现clone功能'

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
 #! /usr/bin/env node
 
-var program = require('commander');
+const program = require('commander');  //命令行开发工具
 
 const path = require('path');
 
-const download = require('download-git-repo');
+const download = require('download-git-repo'); // 模板仓库下载工具
 
+const inquirer = require('inquirer');  //命令行交互工具
+
+const ora = require('ora');  // 命令行中加载状态标识
+
+const chalk = require('chalk');     //命令行输出字符颜色
 
 // 可用模板
 const templates = {
     'reactCli': {
         url: 'https://github.com/1149308443/react',
-        downloadUrl: 'https://github.com/1149308443/react#master',
+        downloadUrl: '1149308443/react#master',
         description: 'my-cli脚手架react'
     },
     'reactReduxCli': {
         url: 'https://github.com/1149308443/react',
-        downloadUrl: 'https://github.com/1149308443/react#dev',
+        downloadUrl: '1149308443/react#dev',
         description: 'my-cli脚手架react-redux'
     },
     'reactMobxCli': {
         url: 'https://github.com/1149308443/react',
-        downloadUrl: 'https://github.com/1149308443/react#dev-mobx',
+        downloadUrl: '1149308443/react#dev-mobx',
         description: 'my-cli脚手架react-mobx'
     },
     'reactTsCli': {
         url: 'https://github.com/1149308443/react',
-        downloadUrl: 'https://github.com/1149308443/react#dev-typescript',
+        downloadUrl: '1149308443/react#dev-typescript',
         description: 'my-cli脚手架react-typesceript'
     }
 }
@@ -35,28 +40,32 @@ const templates = {
 program
     .version('0.0.1', '-v, --version')
     .command('init <template> <project>')
-    .description('初始化项目模板')
+    .description('初始化项目模板工程')
     .action((templateName, projectName) => {
-        console.log(templateName, projectName);
-        // let {downloadUrl} = templates[templateName];        
-        // //第一个参数是github仓库地址，第二个参数是创建的项目目录名，第三个参数是clone
-        // download(downloadUrl, projectName, {clone: true}, err => {
-        //     if(err){
-        //         console.log('下载模板出错');
-        //     }else{
-        //         console.log('下载模板成功');
-        //     }
-        // })
+        let {downloadUrl} = templates[templateName];        
+        //第一个参数是github仓库地址，第二个参数是创建的项目目录名，第三个参数是clone(是clone还是正常下载,可省略) 
+        console.log(downloadUrl);
+        download(downloadUrl, projectName, err => {
+            console.log(downloadUrl, projectName,err);
+            if(err){
+                console.log('下载模板出错');
+            }else{
+                console.log('下载模板成功');
+            }
+        })
     })
-//     .option('-i, --init ,<template>[name]', 'init a project', 'myFirstProject')
 
-    .parse(process.argv);  // 解析命令行参数
-// if(program.init){
-//     // 获取将要构建的项目根目录
-//     var projectPath = path.resolve(program.init);
-//     // 获取将要构建的的项目名称
-//     var projectName = path.basename(projectPath);
 
-//     console.log(program.init);
-//     console.log(projectName);
-// }
+program 
+    .command('list')
+    .description('查看所有可用的项目模板')
+    .action(()=>{
+        console.log({
+            reactCli:'react基础模板',
+            reactReduxCli:'react+redux模板',
+            reactMobxCli:'react+mobx模板',
+            reactTsCli:'react+typescript模板'
+        })
+    })
+
+program.parse(process.argv);  // 解析命令行参数
